055deb9d95b59fbf3fe8d65805bceb96a3676669
KangarooTeam <kangarooteam.py@gmail.com>
2018-08-13 21:44:34 +0300

    добавлен кусок кода, в нем еще неисправленый баг, код рабочий

--- model.py
+++ model.py
@@ -19,4 +19,28 @@ def get_api(teams):
     return result
 
 
-team_api = get_api(result)
+post_api = get_api(result)
+def add_api(team_api):
+    stack = 0
+    cashe = str()
+    new_api = {}
+    for id, name in enumerate(team_api[0]):
+        goal = int(team_api[0][name])
+        if id % 2 == 0 or id == 0:
+            stack = int(goal)
+            cashe = name
+        else:
+            if stack > goal:
+                new_api[name] = [goal, "L", 0]
+                new_api[cashe] = [stack, "W", 3]
+            elif goal > stack:
+                new_api[name] = [goal, "W", 3]
+                new_api[cashe] = [stack, "L", 0]
+            else:
+                new_api[name] = [goal, "D", 1]
+                new_api[cashe] = [stack, "D", 1]
+        stack = 0
+        cashe = str()
+    return new_api
+
+print(add_api(post_api))
